ad3770c5f245bf0fabe16f4d30b0e8107f2502cf
Oleksandr Lysak <lisak.aleksandr@gmail.com>
2024-06-22 22:56:31 +0200

    Опис змін

--- src/Exercise1_2/Main.java
+++ src/Exercise1_2/Main.java
@@ -4,27 +4,28 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class Person {
-    public static String processNames(List<String> names) {
-
-        List<String> filteredNames = IntStream.range(0, names.size())
+public class Main {
+    public static List<String> getNamesAtOddIndices(List<String> names) {
+        return IntStream.range(0, names.size())
                 .filter(i -> i % 2 == 0)
                 .mapToObj(names::get)
                 .collect(Collectors.toList());
+    }
 
-        List<String> uppercasedAndSortedNames = filteredNames.stream()
+    public static List<String> processNames(List<String> names) {
+        return names.stream()
                 .map(String::toUpperCase)
                 .sorted((s1, s2) -> s2.compareTo(s1))
                 .collect(Collectors.toList());
-
-        return IntStream.range(0, uppercasedAndSortedNames.size())
-                .mapToObj(i -> (i * 2 + 1) + ". " + uppercasedAndSortedNames.get(i))
-                .collect(Collectors.joining(", "));
     }
 
     public static void main(String[] args) {
         List<String> names = List.of("Ivan", "Peter", "John", "Mary", "Sophia");
-        String result = processNames(names);
-        System.out.println(result);
+
+        List<String> oddIndexedNames = getNamesAtOddIndices(names);
+        System.out.println(oddIndexedNames);
+
+        List<String> processedNames = processNames(oddIndexedNames);
+        System.out.println(processedNames);
     }
 }
